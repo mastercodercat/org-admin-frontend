2422e90407778457b44495f378dda2b2e837549e
mastercodercat <mastercoderoy64@gmail.com>
2021-12-13 11:39:55 -0800

    Added recover password component tests

--- src/app/login/recover-password/recover-password.component.spec.ts
+++ src/app/login/recover-password/recover-password.component.spec.ts
@@ -1,33 +1,54 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  Spectator,
+  createComponentFactory,
+  byPlaceholder,
+  createRoutingFactory,
+} from '@ngneat/spectator';
 import { FormBuilder } from '@angular/forms';
+import { Location } from '@angular/common';
 import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule } from '@ngrx/store';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { reducers, metaReducers } from 'src/app/store/reducers';
 
 import { RecoverPasswordComponent } from './recover-password.component';
 
 describe('RecoverPasswordComponent', () => {
-  let component: RecoverPasswordComponent;
-  let fixture: ComponentFixture<RecoverPasswordComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ RecoverPasswordComponent ],
-      providers: [
-        FormBuilder,
-      ],
-      imports: [
-        RouterTestingModule,
-      ],
-    })
-    .compileComponents();
+  let spectator: Spectator<RecoverPasswordComponent>;
+  const createComponent = createComponentFactory({
+    component: RecoverPasswordComponent,
+    providers: [FormBuilder],
+    imports: [
+      RouterTestingModule,
+      SharedModule,
+      StoreModule.forRoot(reducers, { metaReducers }),
+    ],
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RecoverPasswordComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => (spectator = createComponent()));
+
+  it('should display recover password ui', () => {
+    expect(spectator.query('.forgot-password-header')).toHaveExactText(
+      'Forgot Password?'
+    );
+    expect(spectator.query('.forgot-password-msg')).toHaveExactText(
+      "Enter the email address you used when you joined and we'll send you instructions to reset your password."
+    );
+    expect(spectator.query('.password-form')).toExist();
+    expect(spectator.query(byPlaceholder('Email address'))).toExist();
+    expect(spectator.query('.reset-password-btn')).toHaveExactText(
+      'Change Password'
+    );
+    expect(spectator.query('.reset-password-btn')).toBeDisabled();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display validation error with invalid email', () => {
+    spectator.typeInElement(
+      'a',
+      spectator.query(byPlaceholder('Email address')) as HTMLElement
+    );
+    expect(spectator.query('.validation-error')).toHaveExactText(
+      'Please enter a valid email'
+    );
   });
 });
