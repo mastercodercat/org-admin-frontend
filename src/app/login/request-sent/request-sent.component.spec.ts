94c2426585b97373aa3e073c907ce00b359e1bb9
mastercodercat <mastercoderoy64@gmail.com>
2021-12-13 14:00:33 -0800

    Added request sent component tests

--- src/app/login/request-sent/request-sent.component.spec.ts
+++ src/app/login/request-sent/request-sent.component.spec.ts
@@ -1,25 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  Spectator,
+  createComponentFactory,
+  byPlaceholder,
+  createRoutingFactory,
+} from '@ngneat/spectator';
+import { FormBuilder } from '@angular/forms';
+import { Location } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule } from '@ngrx/store';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { reducers, metaReducers } from 'src/app/store/reducers';
 
 import { RequestSentComponent } from './request-sent.component';
 
 describe('RequestSentComponent', () => {
-  let component: RequestSentComponent;
-  let fixture: ComponentFixture<RequestSentComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ RequestSentComponent ]
-    })
-    .compileComponents();
+  let spectator: Spectator<RequestSentComponent>;
+  const createComponent = createComponentFactory({
+    component: RequestSentComponent,
+    providers: [FormBuilder],
+    imports: [
+      RouterTestingModule,
+      SharedModule,
+      StoreModule.forRoot(reducers, { metaReducers }),
+    ],
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RequestSentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  beforeEach(() => (spectator = createComponent()));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display ui', () => {
+    expect(spectator.query('h1')).toHaveExactText(
+      'Reset password instructions sent'
+    );
+    expect(spectator.query('p')).toHaveExactText(
+      'An email has been sent over with instructions to reset your password. Please check your email.'
+    );
   });
 });
