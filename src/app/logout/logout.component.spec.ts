7d61ee31444a275e52ad2c9f0cae7126c80d1241
mastercodercat <mastercoderoy64@gmail.com>
2021-12-14 10:16:03 -0800

    Fixed log out component test

--- src/app/logout/logout.component.spec.ts
+++ src/app/logout/logout.component.spec.ts
@@ -1,10 +1,17 @@
+import { RouterTestingModule } from '@angular/router/testing';
 import { Spectator, byText, createRoutingFactory } from '@ngneat/spectator';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from 'src/app/store/reducers';
 import { LogoutComponent } from './logout.component';
 
 describe('LogoutComponent', () => {
   let spectator: Spectator<LogoutComponent>;
   const createComponent = createRoutingFactory({
     component: LogoutComponent,
+    imports: [
+      RouterTestingModule,
+      StoreModule.forRoot(reducers, { metaReducers }),
+    ],
   });
 
   beforeEach(() => {
