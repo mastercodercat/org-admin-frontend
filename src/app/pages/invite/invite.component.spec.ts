786484b0d73bed040b76e30cc827709be4048555
mastercodercat <mastercoderoy64@gmail.com>
2021-12-15 05:23:53 -0800

    Added invite component testing

--- src/app/pages/invite/invite.component.spec.ts
+++ src/app/pages/invite/invite.component.spec.ts
@@ -1,22 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Spectator, byText, createComponentFactory } from '@ngneat/spectator';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from 'src/app/store/reducers';
 
 import { InviteComponent } from './invite.component';
 
 describe('InviteComponent', () => {
-  // let component: InviteComponent;
-  // let fixture: ComponentFixture<InviteComponent>;
-  // beforeEach(async () => {
-  //   await TestBed.configureTestingModule({
-  //     declarations: [ InviteComponent ]
-  //   })
-  //   .compileComponents();
-  // });
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(InviteComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  let spectator: Spectator<InviteComponent>;
+  const createComponent = createComponentFactory({
+    component: InviteComponent,
+    imports: [
+      RouterTestingModule,
+      SharedModule,
+      CommonModule,
+      StoreModule.forRoot(reducers, { metaReducers }),
+      HttpClientModule,
+    ],
+  });
+
+  beforeEach(() => {
+    spectator = createComponent();
+    spectator.detectChanges();
+  });
+
+  it('should display ui', () => {
+    expect(spectator.query(byText('Choose method'))).toExist();
+    expect(spectator.query(byText('Exit'))).toExist();
+  });
 });
