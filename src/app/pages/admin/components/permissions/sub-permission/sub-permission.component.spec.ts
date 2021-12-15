2555dae5b84f5381713f5f4bbda99bea1f6ae385
mastercodercat <mastercoderoy64@gmail.com>
2021-12-15 01:38:53 -0800

    Added sub permission component test

--- src/app/pages/admin/components/permissions/sub-permission/sub-permission.component.spec.ts
+++ src/app/pages/admin/components/permissions/sub-permission/sub-permission.component.spec.ts
@@ -1,24 +1,57 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import {
+  Spectator,
+  byText,
+  createComponentFactory,
+  byPlaceholder,
+} from '@ngneat/spectator';
+import { StoreModule } from '@ngrx/store';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import {
+  ExportOutline,
+  UserOutline,
+  MoreOutline,
+  CaretLeftOutline,
+} from '@ant-design/icons-angular/icons';
+import { reducers, metaReducers } from 'src/app/store/reducers';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 import { SubPermissionComponent } from './sub-permission.component';
 
-describe('SubPermissionComponent', () => {
-  let component: SubPermissionComponent;
-  let fixture: ComponentFixture<SubPermissionComponent>;
+const icons: IconDefinition[] = [
+  ExportOutline,
+  UserOutline,
+  MoreOutline,
+  CaretLeftOutline,
+];
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [SubPermissionComponent],
-    }).compileComponents();
+describe('SubPermissionComponent', () => {
+  let spectator: Spectator<SubPermissionComponent>;
+  const createComponent = createComponentFactory({
+    component: SubPermissionComponent,
+    imports: [
+      StoreModule.forRoot(reducers, { metaReducers }),
+      CommonModule,
+      SharedModule,
+      NzIconModule.forRoot(icons),
+    ],
   });
+  const props = {
+    title: 'SubPermission title',
+    description: 'SubPermission description',
+  };
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SubPermissionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = createComponent({
+      props,
+    });
+    spectator.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display ui', () => {
+    expect(spectator.query(byText(props.title))).toExist();
+    expect(spectator.query(byText(props.description))).toExist();
   });
 });
