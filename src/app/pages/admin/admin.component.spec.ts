4825555379cedd334b51c1c29e0389391c17dbd6
mastercodercat <mastercoderoy64@gmail.com>
2021-12-15 04:12:35 -0800

    Fixed admin component testing

--- src/app/pages/admin/admin.component.spec.ts
+++ src/app/pages/admin/admin.component.spec.ts
@@ -1,26 +1,45 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import {
+  Spectator,
+  byText,
+  createRoutingFactory,
+  createComponentFactory,
+  byPlaceholder,
+  SpectatorRouting,
+} from '@ngneat/spectator';
+import { Router, RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from 'src/app/store/reducers';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { UserOutline, ApartmentOutline } from '@ant-design/icons-angular/icons';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 import { AdminComponent } from './admin.component';
 
-describe('AdminComponent', () => {
-  let component: AdminComponent;
-  let fixture: ComponentFixture<AdminComponent>;
+const icons: IconDefinition[] = [UserOutline, ApartmentOutline];
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AdminComponent],
-      imports: [RouterTestingModule],
-    }).compileComponents();
+describe('AdminComponent', () => {
+  let spectator: Spectator<AdminComponent>;
+  const createComponent = createComponentFactory({
+    component: AdminComponent,
+    providers: [RouterModule],
+    imports: [
+      RouterTestingModule,
+      SharedModule,
+      CommonModule,
+      NzIconModule.forRoot(icons),
+    ],
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AdminComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = createComponent();
+    spectator.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display ui', async () => {
+    await spectator.fixture.whenStable();
+    expect(spectator).toBeTruthy();
   });
 });
