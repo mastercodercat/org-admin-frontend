af7d942f5263dabe7ce06d6adbb37e255c5737ff
mastercodercat <mastercoderoy64@gmail.com>
2021-12-15 01:43:43 -0800

    Added admin component test

--- src/app/pages/admin/admin.component.spec.ts
+++ src/app/pages/admin/admin.component.spec.ts
@@ -1,29 +1,50 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import {
+  Spectator,
+  byText,
+  createComponentFactory,
+  byPlaceholder,
+} from '@ngneat/spectator';
+import { StoreModule } from '@ngrx/store';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import {
+  ExportOutline,
+  UserOutline,
+  MoreOutline,
+  CaretLeftOutline,
+} from '@ant-design/icons-angular/icons';
+import { reducers, metaReducers } from 'src/app/store/reducers';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 import { AdminComponent } from './admin.component';
 
-describe('AdminComponent', () => {
-  let component: AdminComponent;
-  let fixture: ComponentFixture<AdminComponent>;
+const icons: IconDefinition[] = [
+  ExportOutline,
+  UserOutline,
+  MoreOutline,
+  CaretLeftOutline,
+];
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AdminComponent ],
-      imports: [
-        RouterTestingModule,
-      ]
-    })
-    .compileComponents();
+describe('AdminComponent', () => {
+  let spectator: Spectator<AdminComponent>;
+  const createComponent = createComponentFactory({
+    component: AdminComponent,
+    imports: [
+      StoreModule.forRoot(reducers, { metaReducers }),
+      CommonModule,
+      SharedModule,
+      NzIconModule.forRoot(icons),
+    ],
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AdminComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = createComponent();
+    spectator.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display ui', () => {
+    expect(spectator.query(byText('Account Management'))).toExist();
   });
 });
