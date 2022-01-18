e6b70d4341f5133e8ba67a257429706f40a4f573
mastercodercat <mastercoderoy64@gmail.com>
2022-01-17 16:33:48 -0800

    added: enable/disable account feature

--- src/app/pages/admin/components/accounts/account-collapse/account-collapse.component.ts
+++ src/app/pages/admin/components/accounts/account-collapse/account-collapse.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Account } from '../account';
+import {
+  EnableOrganizationGQL,
+  DisableOrganizationGQL,
+} from 'src/app/shared/services/graphql/graphql.service';
 
 @Component({
   selector: 'org-admin-account-collapse',
@@ -12,10 +16,38 @@ export class AccountCollapseComponent implements OnInit {
     'margin-bottom': '24px',
     border: '0px',
   };
+  currentAccount: Account = {} as Account;
 
   @Input() accounts: Account[] = [];
 
-  constructor() {}
+  constructor(
+    private enableOrganization: EnableOrganizationGQL,
+    private disableOrganization: DisableOrganizationGQL
+  ) {}
 
   ngOnInit() {}
+
+  enable(): void {
+    if (this.currentAccount.uuid) {
+      this.enableOrganization
+        .mutate({
+          uuid: this.currentAccount.uuid,
+        })
+        .subscribe();
+    }
+  }
+
+  disable(): void {
+    if (this.currentAccount.uuid) {
+      this.disableOrganization
+        .mutate({
+          uuid: this.currentAccount.uuid,
+        })
+        .subscribe();
+    }
+  }
+
+  setCurrent(account: Account): void {
+    this.currentAccount = account;
+  }
 }
