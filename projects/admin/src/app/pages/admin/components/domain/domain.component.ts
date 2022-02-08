143c496674b3df102de597bd24cfe40e50586179
mastercodercat <mastercoderoy64@gmail.com>
2022-02-08 10:55:52 -0800

    added: NewModal component

--- projects/admin/src/app/pages/admin/components/domain/domain.component.ts
+++ projects/admin/src/app/pages/admin/components/domain/domain.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Domain } from './domain';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'org-admin-domain',
   templateUrl: './domain.component.html',
   styleUrls: ['./domain.component.scss'],
 })
-export class DomainComponent {
+export class DomainComponent implements OnInit {
+  isNewModal = false;
+  validateForm!: FormGroup;
   domains: Domain[] = [
     {
       name: 'action.crowdskout.com',
@@ -33,4 +41,36 @@ export class DomainComponent {
       created_at: '1/6/19 8:12 AM',
     },
   ];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      domain: [null, [Validators.required]],
+    });
+  }
+
+  showNewModal(): void {
+    this.isNewModal = true;
+  }
+
+  handleOk(): void {
+    if (this.validateForm.valid) {
+      console.log('submit', this.validateForm.value);
+      this.isNewModal = false;
+    } else {
+      Object.values(this.validateForm.controls).forEach(
+        (control: AbstractControl) => {
+          if (control.invalid) {
+            control.markAsDirty();
+            control.updateValueAndValidity({ onlySelf: true });
+          }
+        }
+      );
+    }
+  }
+
+  handleClose(): void {
+    this.isNewModal = false;
+  }
 }
